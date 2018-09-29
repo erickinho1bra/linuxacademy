865d501df7c4f9b2af083777d7ab66fa4a1ab24e
Erick Lima <erickdalima@gmail.com>
2018-09-29 20:30:46 +0000

    pushing python test

---
+++ iterating_over_lists.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.6
+
+print("")
+
+company = [
+        { 'name': 'Erick', 'active': True, 'admin': True },
+        { 'name': 'Carol', 'active': True, 'admin': False },
+        { 'name': 'Mom', 'active': False, 'admin': True }
+]
+
+print('print(f"company")')
+print(company)
+print('print(company[1]["name"])')
+print(company[1]['name'])
+print()
+
+
+for user in company:
+    if user
+        print(user['name'])
