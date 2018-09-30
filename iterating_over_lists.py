980247c7663f7d7417a78bf664d31599dcf050c2
Erick Lima <erickdalima@gmail.com>
2018-09-30 04:23:23 +0000

    finished iterating python script

--- iterating_over_lists.py
+++ iterating_over_lists.py
@@ -2,21 +2,25 @@
 
 print("")
 
+count = 1
 company = [
         { 'name': 'Erick', 'active': True, 'admin': True },
         { 'name': 'Carol', 'active': True, 'admin': False },
         { 'name': 'Mom', 'active': False, 'admin': True }
 ]
 
-print('print(f"company")')
-print(company)
-print('print(company[1]["name"])')
-print(company[1]['name'])
-print()
-
-
 for user in company:
-#    print("User: ", user['name'],'\n',"  User is admin? ", user['admin'],'\n',"  Account is active? ", user['active'])
-    print(user['name'])
+    print(count, user['name'])
     if user['admin'] == True:
-        print('(ADMIN)')
+        if user['active'] == True:
+            print('\t', '(ADMIN) - Active')
+        elif user['active'] == False:
+            print('\t', '(ADMIN) - NOT Active')
+    elif user['admin'] == False:
+        if user['active'] == True:
+            print('\t', 'NOT (ADMIN) - Active')
+        elif user['active'] == False:
+            print('\t', 'NOT (ADMIN) - NOT Active')
+    count = count + 1
+
+
