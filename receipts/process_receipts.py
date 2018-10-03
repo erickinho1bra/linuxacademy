08c53086dc6c3ea67104a0f59633ba1a1c548f36
Erick Lima <erickdalima@gmail.com>
2018-10-03 17:23:51 +0000

    auto pushed to github

---
+++ receipts/process_receipts.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.6
+
+import os, glob, json, shutil
+
+try:
+    os.mkdir('./processed')
+except OSError:
+    print("'processed' directory already exitsts")
+
+receipts = glob.glob('./new/receipt-[0-9]*.json')
+subtotal = 0.0
+
+for path in receipts:
+    with open(path) as f:
+        content = json.load(f)
+        subtotal += float(content['value'])
+    name = path.split("/")[-1]
+    destination = f"./processed/{name}"
+    shutil.move(path, destination)
+    print(f"moved '{path}' to '{destination}'")
+
+print("Receipt subtotal: $%.2f" % subtotal)
