78c96b0537fab3c9b7e6f529ade856f0072aa386
Erick Lima <erickdalima@gmail.com>
2018-10-14 03:32:23 +0000

    auto pushed to github

--- fibo.py
+++ fibo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3.6
 
-a = 0
-b = 1
-
-def fib (n, m):
-    while n < m:
+def fib (n, z):
+    m = 1
+    while n < z:
         print(n)
+        n, m = m, m + n
+z = int(input('what would you like the fibo sequence to go till? '))
 
-
+fib(0, z)
