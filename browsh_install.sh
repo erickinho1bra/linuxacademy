99e2a74a87eb872c08b27b22486dd714a3211032
Erick Lima <erickdalima@gmail.com>
2018-12-14 18:30:40 -0500

    auto pushed to github

--- browsh_install.sh
+++ browsh_install.sh
@@ -10,24 +10,55 @@ FIREFOX_EXIST=`hash firefox 2>/dev/null ; echo $?`
 
 # Variable definitions - end
 
-# Script - start
+# Fuction definitions- start
 
 echo ""
 echo "Welcome to the browsh installation script!"
 echo ""
 
+# Function that checks if the necessary prerequisites are installed for browsh
+funcPreReq () {
 if [ $CPU_ARCH == "x86_64" ] ; then
   echo "CPU Architecture is 64bit ($CPU_ARCH)"
   if [ $FIREFOX_EXIST == "0" ] ; then
     echo "Firefox is installed"
+    return 100
   else
     echo "Firefox is not installed"
+    return 101
   fi
 elif [ $FIREFOX_EXIST == "x86" ] ; then
   echo "CPU Architecture is 32bit ($CPU_ARCH)"
+  if [ $FIREFOX_EXIST == "0" ] ; then
+    echo "Firefox is installed"
+    return 110
+  else
+    echo "Firefox is not installed"
+    return 111
+  fi
 else
   echo "Could not determine CPU Architecture"
 fi
+}
+
+# Function to insatll browsh
+funcInstall () {
+if [[ $fPRStatus == 101 ]] ; then
+  echo "All good here"
+else
+  echo "Something went wrong"
+fi
+}
+
+# Function definitions - stop
+
+# Script - start
+
+# Function that checks if the necessary prerequisites are installed for browsh
+funcPreReq
+fPRStatus=$?
+
+funcInstall
 
 # Script - end
 
