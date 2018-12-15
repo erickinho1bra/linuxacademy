4261fc527c40725509fdf2a8036f1fd21b08ddee
Erick Lima <erickdalima@gmail.com>
2018-12-15 17:46:41 -0500

    improved do_the_git_thing.sh script

--- do_the_git_thing.sh
+++ do_the_git_thing.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 git add .
-git commit -m "auto pushed to github"
+
+read -p "What do you want your commit message to be? " COMMESS
+
+if [ "$COMMESS" != "" ] ; then
+  echo "Commiting with message: $COMMESS "
+else
+  COMMESS="Auto commit to github"
+  echo "Using default commit message: $COMMESS"
+fi
+
+git commit -m "$COMMESS"
 git push origin master
