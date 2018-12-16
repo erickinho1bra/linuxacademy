c426f16d7e984dcfa281317fb5441f2201625882
Erick Lima <erickdalima@gmail.com>
2018-12-15 19:02:48 -0500

    test

--- do_the_git_thing.sh
+++ do_the_git_thing.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-git add .
-
 read -p "What do you want your commit message to be? " COMMESS
 
+# Prompt user for git commit message
 if [ "$COMMESS" != "" ] ; then
   echo "Commiting with message: $COMMESS "
 else
@@ -11,13 +10,18 @@ else
   echo "Using default commit message: $COMMESS"
 fi
 
-git commit -m "$COMMESS"
+# Commit changes (if any)
+git commit -a -m "$COMMESS"
+
+# Record exit code
 COMSTATUS=`echo $?`
+
+# Commit depending on whether there are changes to the repository
 if [ "$COMSTATUS" == "1" ] ; then
   echo "Nothing to commit!"
   exit 1
 else
   echo "Committing files (not sins)"
-  git commit -m "$COMMESS"
+  git commit -a -m "$COMMESS"
   git push origin master
 fi
