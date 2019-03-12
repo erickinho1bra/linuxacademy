e3df1c198ae569bf78fd7b5450c477288a83c9dd
Erick Lima <erickdalima@gmail.com>
2019-03-12 15:55:50 -0700

    cleaned up browserOpener.py file

--- browserOpener.py
+++ browserOpener.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-#import webbrowser
 import webbrowser
 
-#sites = ["ellentube-mail.com","ellentube.com","ellentv-mail.com","ellentv.com","games.ellentv.com","m.ellentv.com","media.ellentv.com","new.ellentube.com","photos.ellen.warnerbros.com","share.ellentube.com","tpix-media.ellentv.com","viggle.ellentv.com","widgets.ellentube.com","www-2.ellentube.com","www-new.ellentube.com","www-v2.ellentube.com","www.ellen.warnerbros.com","www.ellentube-mail.com","www.ellentv-mail.com","www.ellentv.com"]
+sites = ["ellentube-mail.com","ellentube.com","ellentv-mail.com","ellentv.com","games.ellentv.com","m.ellentv.com","media.ellentv.com","new.ellentube.com","photos.ellen.warnerbros.com","share.ellentube.com","tpix-media.ellentv.com","viggle.ellentv.com","widgets.ellentube.com","www-2.ellentube.com","www-new.ellentube.com","www-v2.ellentube.com","www.ellen.warnerbros.com","www.ellentube-mail.com","www.ellentv-mail.com","www.ellentv.com"]
 
-sites = ["ellentube-mail.com", "ellentube.com"]
+#sites = ["ellentube-mail.com", "ellentube.com"]
 
-#webbrowser.open('http://inventwithpython.com/')
 for site in sites:
     website = "http://" + site
+    print(f"{website}")
     webbrowser.open(website)
