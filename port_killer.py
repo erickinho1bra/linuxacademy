a2dea1e731e708e3847e4951f946d3c850ce8a9d
Erick Lima <erickdalima@gmail.com>
2019-03-14 14:30:41 -0400

    don't know if this is doing anything

--- port_killer.py
+++ port_killer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3.6
-# <add comments here>
+# python script to kill a process on a given port
 
 ##### global variables - start
 
+import argparse, sys, os
 
+parser = argparse.ArgumentParser(description='Kill process that is using a specific port')
+parser.add_argument('--portnumber', '-p', type=int, help="port number you would like to kill")
+args = parser.parse_args()
+
+PORT_NUMBER = args.portnumber
 
 ##### global variable - stop
 
@@ -19,6 +25,29 @@
 
 ##### script - start
 
+print(f'We are killing process running port {PORT_NUMBER}')
 
+try:
+    result = subprocess.run(
+            ['lsof', '-n', '-i4TCP:%s' % port],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+except subprocess.CalledProcessError:
+    print(f'No process listening on port {port}')
+else:
+    listening = None
+
+    for line in result.stdout.splitlines():
+        if "LISTEN" IN str(line):
+            listening = line
+            break
+
+    if listening:
+        # PID is the second column in the output
+        pid = int(listening.split()[1])
+        os.kill(pid,9)
+        print(f"Killed process {pid}")
+    else:
+        print(f"No process listening on port {port}")
 
 ##### script - stop
