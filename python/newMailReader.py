7ad36040682a89ed2f1d665bc43d6d87b9a6f0cc
Erick Lima <erickdalima@gmail.com>
2019-04-08 15:57:24 -0700

    updated testSAM yaml and python script for emails

--- python/newMailReader.py
+++ python/newMailReader.py
@@ -10,21 +10,8 @@ import sys, signal
 
 ##### global variables - start
 
-# function that handles CTRL+C
-def signal_handler(signal, frame):
-    # print a blank nothing to make prompt go to next line
-    print("")
-    sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-
 ORG_EMAIL   = "@gmail.com"
 user  = "erickdalima" + ORG_EMAIL
-while True:
-    password = getpass("What is your password? ")
-    if password == "":
-        print("You must enter a password!")
-    else:
-        break
 imap_url = "imap.gmail.com"
 SMTP_PORT   = 993
 
@@ -40,13 +27,14 @@ def get_body(msg):
   else:
     return msg.get_payload(None,True)
 
-con = imaplib.IMAP4_SSL(imap_url)
-con.login(user,password)
-con.select('INBOX')
-
-result, data = con.fetch(b'3','(RFC822)')
-raw = email.message_from_bytes(data[0][1])
-print(get_body(raw))
+def main(palavra):
+ con = imaplib.IMAP4_SSL(imap_url)
+ password = palavra
+ con.login(user,password)
+ con.select('INBOX')
+ result, data = con.fetch(b'3','(RFC822)')
+ raw = email.message_from_bytes(data[0][1])
+ print(get_body(raw))
 
 ##### function declerations - stop
 
